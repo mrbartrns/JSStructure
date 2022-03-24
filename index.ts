3b6cd0c83148fdd27ba45d020a3605a9a4bb7da0
Junhyuck Ko <mrbartrns@naver.com>
2022-03-24 14:30:43 +0900

    feat: 트리 전위, 중위, 후위순회 구현

--- index.ts
+++ index.ts
@@ -1,19 +1,29 @@
-import { Queue } from "./LinkedList/Queue";
+import { BST } from "./projects/Tree";
+
+// BST가 잘 작동하는지 검증하는 테스트코드
 
 function test() {
-  const queue = new Queue();
-  queue.enqueue(1);
-  queue.enqueue(2);
-  queue.enqueue(3);
-  queue.enqueue(4);
-  console.log(queue.dequeue());
-  console.log(queue.dequeue());
-  console.log(queue.dequeue());
-  console.log(queue.dequeue());
-  console.log(queue.dequeue());
-  queue.enqueue(5);
-  console.log(queue.peek());
-  console.log(queue.getSize());
+  const dummies = [
+    [7, 6, 9, 1, 5, 10, 3, 4],
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
+    [],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+  dummies.forEach((dummyData) => {
+    const tree = new BST<number>();
+    dummyData.forEach((value) => {
+      tree.insert(value);
+    });
+    console.log("preorder: ");
+    console.log(tree.preorder(tree.root));
+    console.log("inorder: ");
+    console.log(tree.inorder(tree.root));
+    console.log("postorder: ");
+    console.log(tree.postorder(tree.root));
+  });
 }
 
 test();
+
+export { test };
