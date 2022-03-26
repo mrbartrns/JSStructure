812c83a21266c646c284218e0a496fe64d0e6f3c
Junhyuck Ko <mrbartrns@naver.com>
2022-03-26 13:19:17 +0900

    feat: Heap의 형태를 파이썬처럼 변경

--- index.ts
+++ index.ts
@@ -1,26 +1,19 @@
-import { BST } from "./projects/Tree";
-
 // BST가 잘 작동하는지 검증하는 테스트코드
+import heapq from "./structure/Heap";
 function test() {
-  const dummies = [
-    [7, 6, 9, 1, 5, 10, 3, 4],
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    [10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
-    [],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1],
-  ];
-  dummies.forEach((dummyData) => {
-    const tree = new BST<number>();
-    dummyData.forEach((value) => {
-      tree.insert(value);
-    });
-    console.log("preorder: ");
-    console.log(tree.preorder(tree.root));
-    console.log("inorder: ");
-    console.log(tree.inorder(tree.root));
-    console.log("postorder: ");
-    console.log(tree.postorder(tree.root));
-  });
+  const q: number[] = [];
+  heapq.heappush(q, 10);
+  heapq.heappush(q, 9);
+  heapq.heappush(q, 8);
+  heapq.heappush(q, 7);
+  heapq.heappush(q, 6);
+  console.log(heapq.heappop(q));
+  console.log(heapq.heappop(q));
+  console.log(heapq.heappop(q));
+  console.log(heapq.heappop(q));
+  console.log(heapq.heappop(q));
+  console.log(heapq.heappop(q));
+  console.log(heapq.heappop(q));
 }
 
 test();
